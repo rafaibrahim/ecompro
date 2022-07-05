bbce7d9aae97b904f28b18182bd5ead419fd1fd3
rafaibrahim <rafaibrahim99@gmail.com>
2022-07-05 11:33:18 +0530

    commit

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('res', views.fun)
+    path('res', views.fun),
+    path('index', views.index),
+    path('new', views.new,name="new"),
+    path('master', views.master,name="master"),
+
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def fun(request):
     return HttpResponse("hiii")
+
+def index(request):
+    return render(request, 'users/index.html')
+
+def new(request):
+    return render(request, 'users/new.html')
+
+def master(request):
+    return render(request, 'users/master.html')
